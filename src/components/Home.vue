43122aed6b6b719ae32ef0970089aa3608385acb
Misty1636 <a3722041a@yahoo.com.tw>
2019-04-18 23:53:48 +0800

    'New'

---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="index-banner-box">
+      <div class="index-video-box ">
+        <div class="index-video">
+          <div id="bgndVideo" class="player" data-property="{videoURL:'Pc9ZAM7tEPo',containment:'.index-video',autoPlay:true, mute:true, startAt:0, opacity:1,showControls:false}">
+          </div>
+        </div>
+        <!-- <div class="index-video-bg" style="background-image: url('img/');">
+        </div> -->
+        <div class="index-video-mask" style="opacity: 0.0"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted(){
+    $("#bgndVideo").YTPlayer(); 
+  },
+};
+</script>
+
+<style lang="scss">
+.index-video-box {
+   width: 100%;
+   display: block;
+   position: relative;
+   height: 100vh;
+}
+
+.index-video {
+    width: 100%;
+    height: 100%;
+    position: relative; 
+    z-index: 5;
+    display: block;
+}
+
+.index-video-bg {
+    width: 100%;
+    height: 100%;
+    display: block;
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .index-video-mask {
+    width: 100%;
+    height: 100%;
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: #000;
+    opacity: 0.4;
+    z-index: 5;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,80 @@
+// vue套件
 import Vue from "vue";
-import App from "./App.vue";
 
+// Ajax套件
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+import "bootstrap";
+
+// Loading套件
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+
+// fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// jquery
+import $ from "./jquery-vendor";
+import "./jquery.mb.YTPlayer";
+// import VideoBg from "vue-videobg";
+// Vue.component("video-bg", VideoBg);
+
+// 驗證套件
+
+import VeeValidate from "vee-validate";
+import zh_TW from "vee-validate/dist/locale/zh_TW";
+
+// 自訂義套件
+import App from "./App.vue";
+import router from "./router";
+import "./bus";
+import currencyFilter from "./filters/currency";
+import timetampsFilter from "./filters/timetamps";
+
+VeeValidate.Validator.localize("zh_TW", zh_TW);
+Vue.use(VeeValidate, {
+  locale: "zh_TW",
+  events: "input|blur"
+});
+
+library.add(fab, far, fas);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
 
+Vue.component("Loading", Loading);
+Vue.filter("currency", currencyFilter);
+Vue.filter("getTime", timetampsFilter);
+
+axios.defaults.withCredentials = true;
+window.$ = $;
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount("#app");
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.VUE_APP_API}/api/user/check`;
+
+    axios.post(api).then(response => {
+      // console.log(response.data)
+      if (response.data.success) {
+        next();
+      } else {
+        next({
+          path: "/login"
+        });
+      }
+    });
+  } else {
+    next();
+  }
+});
