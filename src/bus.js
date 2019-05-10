a77749ff977305a34563bfa44040881568b3009b
Misty1636 <a3722041a@yahoo.com.tw>
2019-05-11 03:08:29 +0800

    update-pages

--- src/bus.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-
-Vue.prototype.$bus = new Vue();
-
-// 自定義名稱 'messsage:push'
-// this.$bus.$emit('message:push',message,status)
-// message(string): 傳入參數(內容)
-// status:(string): Alert樣式
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  publicPath: '/WorldTravel/',
-  outputDir: 'dist'
-};
-
